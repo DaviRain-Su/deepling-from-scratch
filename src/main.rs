26003464aafe742c6606779ba11db1466b58989c
Davirain <davirain.yin@gmail.com>
2025-03-21 19:14:17 +0800

    add and_ndarray function impl by ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ndarray::Array1;
 fn main() {
     let result = and(1.0, 1.0);
     println!("and(1.0, 1.0) - Result: {}", result);
@@ -7,10 +8,29 @@ fn main() {
     println!("and(0.0, 1.0) - Result: {}", result);
     let result = and(0.0, 0.0);
     println!("and(0.0, 0.0) - Result: {}", result);
+
+    let result = and_ndarray(1.0, 1.0);
+    println!("and_ndarray(1.0, 1.0) - Result: {}", result);
+    let result = and_ndarray(1.0, 0.0);
+    println!("and_ndarray(1.0, 0.0) - Result: {}", result);
+    let result = and_ndarray(0.0, 1.0);
+    println!("and_ndarray(0.0, 1.0) - Result: {}", result);
+    let result = and_ndarray(0.0, 0.0);
+    println!("and_ndarray(0.0, 0.0) - Result: {}", result);
 }
 
 fn and(x1: f64, x2: f64) -> f64 {
-    let (w1, w2, theta) = (0.5, 0.5, 0.7);
-    let tmp = x1 * w1 + x2 * w2;
-    if tmp <= theta { 0.0 } else { 1.0 }
+    let (w1, w2, b) = (0.5, 0.5, -0.7);
+    let tmp = x1 * w1 + x2 * w2 + b;
+    if tmp <= 0.0 { 0.0 } else { 1.0 }
+}
+
+// use ndarray impl and function
+fn and_ndarray(x1: f64, x2: f64) -> f64 {
+    let x = Array1::from(vec![x1, x2]);
+    let w = Array1::from(vec![0.5, 0.5]);
+    let b = -0.7;
+    let tmp = x.dot(&w) + b;
+    dbg!(tmp);
+    if tmp <= 0.0 { 0.0 } else { 1.0 }
 }
