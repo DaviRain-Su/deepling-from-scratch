c2caa663d37d7b6dc3a5d0b0f0da5316e9684beb
Davirain <davirain.yin@gmail.com>
2025-03-21 21:14:36 +0800

    Update plt_function.py

--- py/plt_function.py
+++ py/plt_function.py
@@ -10,9 +10,14 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
 x = np.arange(-5.0, 5.0, 0.1)
 # y = step_function(x)
-y = sigmoid(x)
+# y = sigmoid(x)
+y = relu(x)
 plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
+# plt.ylim(-0.1, 1.1)
 plt.show()
